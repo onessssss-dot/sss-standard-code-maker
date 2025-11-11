dcb1317404bdf7e8d2065590cd9ed07a71fe4a08
onessssss-dot <2721092936@qq.com>
2025-11-11 16:15:00 +0800

    完成应用基本功能

---
+++ backend/codemaker/src/main/java/com/example/demo/model/dto/app/AppAdminUpdateRequest.java
@@ -0,0 +1,35 @@
+package com.example.demo.model.dto.app;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * 管理员更新APP接口
+ *
+ */
+@Data
+public class AppAdminUpdateRequest implements Serializable {
+
+    /**
+     * id
+     */
+    private Long id;
+
+    /**
+     * 应用名称
+     */
+    private String appName;
+
+    /**
+     * 应用封面
+     */
+    private String cover;
+
+    /**
+     * 优先级
+     */
+    private Integer priority;
+
+    private static final long serialVersionUID = 1L;
+}
--- backend/codemaker/src/main/java/com/example/demo/service/AppService.java
+++ backend/codemaker/src/main/java/com/example/demo/service/AppService.java
@@ -1,7 +1,16 @@
 package com.example.demo.service;
 
+import cn.hutool.core.bean.BeanUtil;
+import com.example.demo.exception.BusinessException;
+import com.example.demo.exception.ErrorCode;
+import com.example.demo.model.dto.app.AppQueryRequest;
+import com.example.demo.model.vo.AppVO;
+import com.example.demo.model.vo.UserVO;
+import com.mybatisflex.core.query.QueryWrapper;
 import com.mybatisflex.core.service.IService;
 import com.example.demo.model.entity.App;
+
+import java.util.List;
 
 /**
  *  服务层。
@@ -9,5 +18,30 @@ import com.example.demo.model.entity.App;
  * @author <a>SSS</a>
  */
 public interface AppService extends IService<App> {
+
+    /**
+     * 获取应用封装类
+     * @param app
+     * @return
+     */
+    AppVO getAppVO(App app);
+
+
+    /**
+     * 获取应用封装类列表
+     *
+     * @param appList
+     * @return
+     */
+    List<AppVO> getAppVOList(List<App> appList);
+
+    /**
+     * 构造应用 查询条件
+     * @param appQueryRequest
+     * @return
+     */
+     QueryWrapper getQueryWrapper(AppQueryRequest appQueryRequest);
+
+
 
 }
